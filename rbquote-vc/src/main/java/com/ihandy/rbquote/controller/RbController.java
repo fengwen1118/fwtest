2dcc1ccb48dadc92380c857fdb1d54f46a893084
fengwen1118 <fengwen1118@163.com>
2016-04-29 09:14:34 +0800

    update

--- rbquote-vc/src/main/java/com/ihandy/rbquote/controller/RbController.java
+++ rbquote-vc/src/main/java/com/ihandy/rbquote/controller/RbController.java
@@ -1,8 +1,64 @@
 package com.ihandy.rbquote.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by fengwen on 2016/4/28.
  */
+@Controller
+@RequestMapping("/Rb")
 public class RbController {
+    private static Logger logger = LoggerFactory.getLogger(RbController.class);
+
+
+    /**
+     * 查询续保信息
+     *
+     * @param LicenseNo
+     * @param CityCode
+     * @param CustKey
+     * @param SecCode
+     * @return
+     */
+    @RequestMapping("/getRenewalInfo")
+    @Transactional
+    @ResponseBody
+    public Map<String, Object> getRenewalInfo( String LicenseNo, int CityCode,String CustKey,String SecCode) {
+
+        try {
+
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    /**
+     *  根据车牌号获取车架号、发动机号、品牌型号接口
+     * @param LicenseNo
+     * @return
+     */
+    @RequestMapping("/getCarInfoByLicenseNo")
+    @Transactional
+    @ResponseBody
+    public Map<String, Object> getCarInfoByLicenseNo( String LicenseNo) {
+        Map map =new HashMap();
+        try {
+
+        } catch (Exception e) {
+
+        }
+        return map;
+    }
+
+
 
 }
