f9608b0a2cf6af15cc8969e02b782a6dbec7c2f6
fengwen1118 <fengwen1118@163.com>
2016-05-05 18:36:59 +0800

    update

---
+++ quote-common/src/main/java/com/ihandy/quote_common/jedis/RedisTableNameConstant.java
@@ -0,0 +1,14 @@
+package com.ihandy.quote_common.jedis;
+
+public class RedisTableNameConstant {
+
+	/** 车辆信息表*/
+	public static final String CHELENET_CHEXIAN_SEARCH_CARINFO = "cn_cx_s_car";
+	/** 关系人员表*/
+	public static final String CHELENET_CHEXIAN_SEARCH_PEOPLEINFO = "cn_cx_s_people";
+	/** 车辆出险表*/
+	public static final String CHELENET_CHEXIAN_SEARCH_CLIAMINFO = "cn_cx_s_cliam";
+	/** 险种报价表*/
+	public static final String CHELENET_CHEXIAN_SEARCH_QUOTEINFO = "cn_cx_s_quote";
+	
+}
